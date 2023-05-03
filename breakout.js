b641f61e9f37fc24b4f91fd980eb6c2dcfa0655f
Josh <jwgorrnado@gmail.com>
2023-05-03 17:16:43 -0400

    updated canvas size

--- breakout.js
+++ breakout.js
@@ -1,8 +1,8 @@
 /// <reference path = "p5.global.d.ts"/>
 
 class Game {
-  width = windowWidth;
-  height = windowHeight;
+  width = windowWidth - 50;
+  height = windowHeight - 50;
   lives = 3;
   score = 0;
   targetRows = random(2, 5);
